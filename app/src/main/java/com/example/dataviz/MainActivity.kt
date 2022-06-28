b43f59d34e33d4e441f054c626429dd5d826d945
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-06-28 14:11:02 +0530

    added spinner but not implemented yet

--- app/src/main/java/com/example/dataviz/MainActivity.kt
+++ app/src/main/java/com/example/dataviz/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.dataviz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button:Button
+    private lateinit var spinner: Spinner
+    var courses = arrayOf<String?>("a.csv","b.csv")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         spinner =findViewById(R.id.spinner)
+        button =findViewById(R.id.button)
+
     }
 }
