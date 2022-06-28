f6c37251cc7b8e712e3700bdf49e7648dd78db7c
Your Name <akum19cs@cmrit.ac.in>
2022-06-28 19:28:33 +0530

    spinner content detected

--- app/src/main/java/com/example/dataviz/MainActivity.kt
+++ app/src/main/java/com/example/dataviz/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.dataviz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button:Button
     private lateinit var spinner: Spinner
+    private lateinit var data:TextView
     var courses = arrayOf<String?>("a.csv","b.csv")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        data =findViewById(R.id.sampleData)
          spinner =findViewById(R.id.spinner)
         button =findViewById(R.id.button)
     val adapter = ArrayAdapter.createFromResource(this,R.array.csv,android.R.layout.simple_spinner_item)
@@ -21,7 +21,25 @@ class MainActivity : AppCompatActivity() {
         spinner.adapter = adapter
 
 
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val text: String = spinner.selectedItem.toString()
+                data.text = text
+            }
+
+        }
+
+
     }
 }
+
+
+
+
 
 
